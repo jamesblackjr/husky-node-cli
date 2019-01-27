e8a437c70e667a3d342af3191449ef94fe1eac86
James A. Black Jr <mrdesigner@jamesblackjr.com>
2019-01-26 22:25:11 -0600

    Create simple CLI wrapper for Husky.

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
-# husky-cli
-A Node CLI interface for Husky - Git Hooks made easy, woof!
+# Husky CLI
+
+A Node CLI interface for [Husky](https://github.com/typicode/husky) - Git Hooks made easy, woof!
+
+## Disclaimer
+
+**Warning**: This CLI tool is still a work in progress, use with caution!
+
+## Installation
+
+```bash
+npm install -g husky-cli
+
+cd git-project
+
+husky install
+```
--- index.js
+++ index.js
@@ -3,4 +3,19 @@
 const husky = require('./node_modules/husky/lib/installer');
 const path = require('path');
 
-husky.install(path.resolve(__dirname, 'node_modules/husky'));
+process.env.HUSKY_CURRENT_DIR = true;
+process.env.HUSKY_SKIP_INSTALL = false;
+
+const [, , action, huskyDir = path.resolve(__dirname, 'node_modules/husky')] = process.argv;
+
+try {
+  // Run installer
+  if (action === 'install' || null) {
+    husky.install(huskyDir);
+  } else if (action === 'uninstall') {
+    husky.uninstall(huskyDir);
+  }
+} catch (error) {
+  console.log(`husky > failed to ${action}`)
+  console.log(error.message)
+}
